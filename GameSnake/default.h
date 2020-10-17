dbe835595b6fe97cdbdee667dd7dbb5a803e1295
Will Tatam <you@example.com>
2020-10-17 12:28:52 +0100

    Tidy up LED connection details

--- GameSnake/default.h
+++ GameSnake/default.h
@@ -3,23 +3,19 @@
 #ifdef ESP32
 #define FASTLED_ALLOW_INTERRUPTS 0
 // Params for width and height
-const uint8_t kMatrixWidth = 8; // length of string
+const uint8_t kMatrixWidth = 8;
 const uint8_t kMatrixHeight = 8;
 bool kMatrixSerpentineLayout = false;
 #else
 // Params for width and height
-const uint8_t kMatrixWidth = 30; // length of string
+const uint8_t kMatrixWidth = 30;
 const uint8_t kMatrixHeight = 30;
 bool kMatrixSerpentineLayout = true;
 #endif
 
-
 /*
    Default config file with normal FastLED stuff
 */
-#define DATA_PIN    2
-//#define CLK_PIN   4
-#define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 #define NUM_LEDS    64
 CRGB leds[NUM_LEDS];
@@ -27,7 +23,9 @@ CRGB leds[NUM_LEDS];
 void ledSetup() {
 
 #ifdef ESP32
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  #define LED_TYPE WS2811
+  #define LED_PIN  2
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
 #else
   #define LED_PIN 7
   #define CLOCK_PIN 14
